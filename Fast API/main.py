68a641eb19ea82c59caedf48b6ff3f8f0bde4f1d
KushDev19 <kushrank24680@gmail.com>
2024-10-03 06:41:28 -0400

    "Uncommented and implemented read_transactions endpoint, added import for List from typing"

--- Fast API/main.py
+++ Fast API/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException, Depends
-from typing import Annotated
+from typing import Annotated, List
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from database import SessionLocal, engine
@@ -50,6 +50,7 @@ async def create_transaction(transaction: TransactionBase, db: db_dependency):
     db.refresh(db_transaction)
     return db_transaction
 
-# @app.get("/transactions", response_model=List[TransactionModel])
-# async def read_transactions(db: db_dependency, skip: int = 0, limit: int = 100):
-#     transactions = db.query
+@app.get("/transactions", response_model=List[TransactionModel])
+async def read_transactions(db: db_dependency, skip: int = 0, limit: int = 100):
+    transactions = db.query(models.Transaction).offset(skip).limit(limit).all()
+    return transactions
